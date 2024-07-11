4988d98ecd4783c29bca40df46764462b2e7fc17
vineas <alvienasyandika@gmail.com>
2024-07-11 23:26:20 +0700

    first commit

--- 3_operator.dart
+++ 3_operator.dart
@@ -1,3 +1,20 @@
 main(){
-    
+    int a = 10;
+    int b = 5;
+
+    //operator aritmatika
+    print("Operator Aritmatika");
+    print(a + b);
+    print(a - b);
+    print(a * b);
+    print(a / b);
+    print(a % b);
+
+    //operator logika
+    print("Operator Logika");
+    bool isTrue = true;
+    bool isFalse = false;
+    print('isTrue && isFalse = ${isTrue && isFalse}');
+    print('isTrue || isFalse = ${isTrue || isFalse}');
+    print('!isTrue = ${!isTrue}');
 }
---
+++ 4_conditional.dart
@@ -0,0 +1,19 @@
+main(){
+    int nilai = 86;
+
+    if(nilai >= 90){
+        print("Nilai A");
+    }
+    else if(nilai >= 80){
+        print("Nilai B");
+    }
+    else if(nilai >= 70){
+        print("Nilai C");
+    }
+    else if(nilai >= 60){
+        print("Nilai D");
+    }
+    else{
+        print("Nilai E");
+    }
+}
